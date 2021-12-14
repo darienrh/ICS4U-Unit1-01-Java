ff158066c58bc7c6382d9db2b8e83f5746303240
darienrh <94391400+darienrh@users.noreply.github.com>
2021-12-14 23:17:20 +0000

    tabs

--- Main.java
+++ Main.java
@@ -11,28 +11,28 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+ public static void main(String[] args) {
 
-	float logLength = 0;
+ float logLength = 0;
 
-	float totalLogs = 0;
+ float totalLogs = 0;
 
-	int logWeightPerMeter = 20; //20 kg per meter
+ int logWeightPerMeter = 20; //20 kg per meter
 
-	int maxTruckLoad = 1100; // 1100 kg per truck
+ int maxTruckLoad = 1100; // 1100 kg per truck
 
-	float maxMeterLogs = maxTruckLoad/logWeightPerMeter;  //Maximum number of meters per load
+ float maxMeterLogs = maxTruckLoad/logWeightPerMeter;  //Maximum number of meters per load
 
-	Scanner myInput = new Scanner (System.in);  //define instance of Scanner for input
+ Scanner myInput = new Scanner (System.in);  //define instance of Scanner for input
 
-	System.out.print("Enter the length of the logs for this load: ");
+ System.out.print("Enter the length of the logs for this load: ");
 
-	logLength = myInput.nextFloat();  //get length of log
+ logLength = myInput.nextFloat();  //get length of log
 
-	totalLogs = maxMeterLogs/logLength;   // Using log length calculate maximum capacity.
+ totalLogs = maxMeterLogs/logLength;   // Using log length calculate maximum capacity.
 
-	System.out.println("Total number of logs this truck can carry = "+ totalLogs);
+ System.out.println("Total number of logs this truck can carry = "+ totalLogs);
 
-	}
+ }
 
 }
