1f0519324660d8b6641d9650718e5ef66ab30c7b
darienrh <94391400+darienrh@users.noreply.github.com>
2021-12-14 23:27:46 +0000

    javadoc thing and end with new line

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-/*
+/**
 * The "Hello Word!" program, just proves you can show print to terminal.
 *
 * @author  Darien R-H
@@ -17,19 +17,19 @@ public class Main {
 
  float totalLogs = 0;
 
- int logWeightPerMeter = 20; //20 kg per meter
+ int logWeightPerMeter = 20;//20 kg per meter
 
- int maxTruckLoad = 1100; // 1100 kg per truck
+ int maxTruckLoad = 1100;//1100 kg per truck
 
- float maxMeterLogs = maxTruckLoad/logWeightPerMeter;  //Maximum number of meters per load
+ float maxMeterLogs = maxTruckLoad / logWeightPerMeter;//Maximum number of meters per load
 
  Scanner myInput = new Scanner (System.in);  //define instance of Scanner for input
 
  System.out.print("Enter the length of the logs for this load: ");
 
- logLength = myInput.nextFloat () ;  //get length of log
+ logLength = myInput.nextFloat();//get length of log
 
- totalLogs = maxMeterLogs / logLength;   // Using log length calculate maximum capacity.
+ totalLogs = maxMeterLogs / logLength;// Using log length calculate maximum capacity.
 
  System.out.println ("Total number of logs this truck can carry = " + totalLogs);
 
